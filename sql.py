d3bddf45ae7888c43f7deccf1bc6cfbfadeae9db
D3Ti <62066592+TheD3Ti@users.noreply.github.com>
2023-04-14 05:52:51 -0700

    Create sql.py

---
+++ sql.py
@@ -0,0 +1,9 @@
+import requests
+import re
+a = input("Target: ")
+url = (a + "%27")
+req = requests.get(url)
+if re.search('(?i)SQL',req.text):
+    print(a + '   Yes. This Site Have a SQL Vulnerability')
+else:
+    print(a + '   No. This Site Doesnt Have a SQL Vulnerability')
